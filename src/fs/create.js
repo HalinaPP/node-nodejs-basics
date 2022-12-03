d2c84441f93ce3bea3262ec0f7bba4aeafa9431a
Halina Pishchala <halina_pishchala@epam.com>
2022-12-03 17:28:09 +0300

    implemented create fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile, existsSync } from 'fs';
+
+const FILES_PATH = './src/fs/files/';
+
 const create = async () => {
-    // Write your code here 
+    const fileName = `${FILES_PATH}fresh.txt`;
+
+    try {
+        if (existsSync(fileName)) {
+            throw new Error('FS operation failed');
+        } else {
+            await writeFile(fileName, 'I am fresh and young', 'utf-8', (err) => {
+                if (err) throw err.message;
+                console.log('The file has been saved!');
+            });
+        }
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 await create();
