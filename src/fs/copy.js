e883f1506bbcb71fb95eca538fe7172d670b2bd3
Halina Pishchala <halina_pishchala@epam.com>
2022-12-03 17:28:40 +0300

    implemented copy fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { cp, existsSync, mkdir } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    const source = './src/fs/files/';
+    const dest = './src/fs/files_copy/';
+    try {
+        if (existsSync(dest) || !existsSync(source)) {
+            throw new Error('FS operation failed');
+        }
+
+        await mkdir(dest, async (err) => {
+            if (err) {
+                throw new Error('mkdir operation failed:', err.message);
+            }
+
+            await cp(source, dest, { recursive: true }, (err) => {
+                if (err) {
+                    throw new Error('copy operation failed:', err.message);
+                }
+                console.log(`folder ${source} was copied to ${dest}`);
+            });
+
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 copy();
