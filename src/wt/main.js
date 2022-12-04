26966af23fe8820fe7e6bbb36c84edddcbac897b
Halina Pishchala <halina_pishchala@epam.com>
2022-12-04 21:48:36 +0300

    feat:implement Worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const cpu = cpus();
+
+const workerName = new URL('./worker.js', import.meta.url);
+
+const runCalculation = (numberToCalcFibonacci) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(workerName, { workerData: numberToCalcFibonacci });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    });
+};
+
+const getWorkerResult = (items) => {
+    const result = [];
+
+    items.map(item => {
+        const status = item.status === 'fulfilled' ? 'resolved' : 'error';
+        const data = item.status === 'fulfilled' ? item.value : null;
+        result.push({ status, data });
+    });
+
+    return result;
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    let numberToCalcFibonacci = 10;
+
+    try {
+        const calculationResult = await Promise.allSettled(cpu.map(() => {
+            return runCalculation(numberToCalcFibonacci++);
+        }));
+
+        const workerResult = getWorkerResult(calculationResult);
+
+        console.log('Workers result:', workerResult);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await performCalculations();
