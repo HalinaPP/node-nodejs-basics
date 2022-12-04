f5b2b0e64c071dcd1b82d5e240094dcbc2481f10
Halina Pishchala <halina_pishchala@epam.com>
2022-12-04 04:39:29 +0300

    feat: implement streams task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { stdin, stdout } from 'process';
+import { pipeline, Transform } from 'stream';
+import { EOL } from 'os';
+
+const reverseString = new Transform({
+    transform(chunk, encoding, cb) {
+        const stringForTransform = chunk.toString().replace(EOL, '');
+        const revertedString = stringForTransform.split('').reverse().join('') + EOL;
+
+        cb(null, revertedString);
+    }
+});
+
 const transform = async () => {
-    // Write your code here 
+    console.log('Type something for reversing:');
+    pipeline(
+        stdin,
+        reverseString,
+        stdout,
+        (err) => {
+            if (err) {
+                console.log('Error during reverting string:', err);
+            }
+        });
 };
 
 await transform();
