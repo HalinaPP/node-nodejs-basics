c55252de49f2b5dc4ed2be119fc72f3c573e9912
Halina Pishchala <halina_pishchala@epam.com>
2022-12-04 03:55:01 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+const { createHash } = await import('node:crypto');
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileToCalculateHashFor = new URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+
+    const hash = createHash('sha256');
+    const input = createReadStream(fileToCalculateHashFor);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
