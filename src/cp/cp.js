d27db0a5e32b78f0be523bbbf67b5b58cf1e2cd4
Halina Pishchala <halina_pishchala@epam.com>
2022-12-05 01:43:47 +0300

    feat: implement Child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import { spawn } from 'child_process';
+import { stdin } from 'process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptFile = new URL('./files/script.js', import.meta.url);
+    const scriptFilePath = scriptFile.pathname.slice(1);
+
+    const argsArr = args.split(' ');
+
+    const childProcess = spawn(`node`, [scriptFilePath, ...argsArr]);
+
+    stdin.on('data', (input) => {
+        childProcess.stdin.write(input);
+    });
+
+    childProcess.stdout.on('data', (output) => {
+        console.log(output.toString());
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess('--arg1 value1 --arg2 value2 --arg3 value3');
