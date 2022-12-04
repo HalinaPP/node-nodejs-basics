e0236693770c6f05a73e0a3116ddf076cd16a8e7
Halina Pishchala <halina_pishchala@epam.com>
2022-12-04 14:32:55 +0300

    feat: implement command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const outputArgs = args.reduce((output, arg, index, arr) => {
+        const nextArg = arr[index + 1];
+
+        if (nextArg && arg.startsWith('--')) {
+            const argName = arg.slice(2);
+            output.push(`${argName} is ${nextArg}`);
+        }
+
+        return output;
+    }, []);
+
+    console.log(outputArgs.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envKeys = Object.keys(process.env);
+    const rssEnvKeys = envKeys.filter(item => item.startsWith('RSS_'));
+
+    const rssEnvsArr = rssEnvKeys.reduce((ouputArr, rssEnvKey) => {
+        ouputArr.push(`${rssEnvKey}=${process.env[rssEnvKey]}`);
+        return ouputArr;
+    }, []);
+
+    const rssEnvs = rssEnvsArr.join('; ');
+
+    console.log(rssEnvs);
 };
 
 parseEnv();
